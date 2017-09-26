2d1d43f529b68321bb64665c9489544d173cfcf7
j1301 <j1301@PC4-33.cc.matsue-ct.jp>
2017-09-26 12:01:31 +0900

    add new method

--- vector.rb
+++ vector.rb
@@ -1,14 +1,18 @@
 class Vector
+  attr_accessor :x, :y
   def initialize(x, y)
     @x = x
     @y = y
   end
 
   def to_s
-    "(#{@x}, #{@y})"
+   "(#{@x}, #{@y})"
   end
 
   def length
     Math.sqrt(@x**2 + @y**2)
+  end
+  def add(name)
+    Vector.new(name.x+@x, name.y+@y)
   end
 end
